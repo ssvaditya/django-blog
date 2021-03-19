5e6ea946eb2d62907a8301ac24e386fa0849aab1
ssvaditya <ssvaditya@gmail.com>
2021-03-19 16:23:16 +0530

    Initial Commit

---
+++ blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
+    body = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return f"{self.post.title} - {self.name}"
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.post.pk})
+
