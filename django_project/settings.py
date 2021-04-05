137cf42867e3781ac275f0d445c17ae1a386058d
ssvaditya <ssvaditya@gmail.com>
2021-04-05 22:50:52 +0530

    Updated Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ="+i_8i!3ubch@33tw+8i^s3*nqkmr*)x5!(8&_@l=98jnuxok#t" 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['schaferdjangoblogapp.herokuapp.com']
 
 
 # Application definition
